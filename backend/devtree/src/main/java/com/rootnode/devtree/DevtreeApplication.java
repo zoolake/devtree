6981ac33a380db4c1e5b4090d5cee407ecbc6d19
문준호 <junhomoon224@gmail.com>
2022-01-26 15:34:56 +0900

    [BE]init : 초기설정

---
+++ backend/devtree/src/main/java/com/rootnode/devtree/DevtreeApplication.java
@@ -0,0 +1,13 @@
+package com.rootnode.devtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtreeApplication.class, args);
+	}
+
+}

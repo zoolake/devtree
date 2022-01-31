a351c84677e733a3d512a7143df2a96511d179e9
all-eviate <riverboy94@gmail.com>
2022-01-31 15:32:58 +0900

    [FE] feat : Suggest component structure of community page
    CommunitySection uses CommunitySectionRow components

--- frontend/skeleton-code/src/components/views/CommunityPage/CommunityPage.js
+++ frontend/skeleton-code/src/components/views/CommunityPage/CommunityPage.js
@@ -1,16 +1,58 @@
 import React from "react";
 import { FaCommentAlt } from "react-icons/fa";
 
+function CommunitySectionRow() {
+  return (
+    <div style={{
+      display:'flex',
+      width:'100%',
+      backgroundColor:'beige',
+      borderRadius:10,
+      flexDirection:'row',
+      justifyContent:'space-between',
+      fontSize:16,
+      padding:'10px 20px',
+      margin:'10px 0px',
+    }}>
+      <div>기술스택</div>
+      <div>제목</div>
+      <div>조회 수</div>
+      <div>작성자</div>
+      <div>작성일자</div>
+      <div>좋아요 수</div>
+    </div>
+  )
+}
+
+function CommunitySection() {
+  return (
+    <div style={{
+      width:'600px',
+      backgroundColor:'tomato',
+      borderRadius:10,
+      padding:'10px 20px',
+    }}>
+      <CommunitySectionRow />
+      <CommunitySectionRow />
+      <CommunitySectionRow />
+      <CommunitySectionRow />
+      <CommunitySectionRow />
+    </div>
+  )
+}
+
 function CommunityPage() {
   return (
-    <>
-      <div className="app">
+    <div style={{
+      width:'1000px'
+    }}>
+      <CommunitySection />
+      {/* <div className="app">
         <FaCommentAlt style={{ fontSize: "4rem" }} />
         <br />
         <span style={{ fontSize: "2rem" }}>Community Page!</span>
-      </div>
-      <div style={{ float: "right" }}></div>
-    </>
+      </div> */}
+    </div>
   );
 }
 

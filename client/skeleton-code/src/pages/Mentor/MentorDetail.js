f9eacd7001bdc0281b19ad27a1d6731f1488f3bf
all-eviate <riverboy94@gmail.com>
2022-02-04 00:39:32 +0900

    [FE] feat : 멘토 디테일 페이지 구조분해할당을 시도했으나 실패하고 객체 속성으로 접근

--- client/skeleton-code/src/pages/Mentor/MentorDetail.js
+++ client/skeleton-code/src/pages/Mentor/MentorDetail.js
@@ -1,18 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function MentorDetail() {
   const { id } = useParams();
+  const [mentor, setMentor] = useState();
   const getMentor = async () => {
     const json = await (
       await fetch(`https://61fa456931f9c20017596733.mockapi.io/mentors/${id}`)
     ).json();
-    console.log(json);
+    // console.log(json);
+    setMentor(json);
   };
   useEffect(() => {
     getMentor();
   }, []);
 
-  return <h1>{id}</h1>;
+  console.log(mentor);
+
+  return (
+    <div>
+      <h1>{mentor.user_name}</h1>
+      <h2>{mentor.mentor_seq}</h2>
+    </div>
+  );
 }
 export default MentorDetail;

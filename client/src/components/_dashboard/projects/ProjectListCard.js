e66501028e615ba589aef8158dc5fbd5fd8bdde8
Hans Jo <wngkd441@gmail.com>
2022-02-16 14:52:52 +0900

    [FE] feat : 프로젝트 리스트 디자인 레이아웃
    프로젝트 리스트 디자인 레이아웃

--- client/src/components/_dashboard/projects/ProjectListCard.js
+++ client/src/components/_dashboard/projects/ProjectListCard.js
@@ -1,29 +1,93 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+//
+import { Card, CardContent, Typography, Stack } from '@mui/material';
 
 ProjectListCard.propTypes = {
   project: PropTypes.object.isRequired
 };
 
 export default function ProjectListCard({ project }) {
+  // STATE
+  const TEAM_STATE = [
+    { state: 'RECRUIT', stateName: '모집 중' },
+    { state: 'COMPLETED', stateName: '모집 완료됨' },
+    { state: 'FINISH', stateName: '종료됨' }
+  ];
+
+  // FUNC
+  const showTechs = project.teamTech.map((tech) => <div key={tech.techSeq}>{tech.techName}</div>);
+  const getStateName = () => {
+    for (let i = 0; i < TEAM_STATE.length; i += 1) {
+      if (TEAM_STATE[i].state === project.teamState) {
+        return TEAM_STATE[i].stateName;
+      }
+    }
+  };
+  const showState = () => {
+    if (project.teamState === 'RECRUIT') {
+      return <Typography color="blue">{getStateName()}</Typography>;
+    }
+    if (project.teamState === 'COMPLETED') {
+      return <Typography color="primary">{getStateName()}</Typography>;
+    }
+    return <Typography color="red">{getStateName()}</Typography>;
+  };
+
   // PAGE
-  if (!project.teamPosition || !project.teamTech) return <div>'로딩 중'</div>;
+  if (!project.teamTech) return <div>'로딩 중'</div>;
   return (
     <ul>
-      <li>
-        <Link to={`${project.teamSeq}`}>
-          <div>'teamManagerName': {project.teamManagerName}</div>
-          <div>'teamManagerSeq': {project.teamManagerSeq}</div>
-          <div>'teamMemberCnt': {project.teamMemberCnt}</div>
-          <div>'teamName': {project.teamName}</div>
-          <div>'teamPosition': {JSON.stringify(project.teamPosition.map((pos) => pos))}</div>
-          <div>'teamRecruitCnt': {project.teamRecruitCnt}</div>
-          <div>'teamSeq': {project.teamSeq}</div>
-          <div>'teamState': {project.teamState}</div>
-          <li>'teamTech': {JSON.stringify(project.teamTech.map((pos) => pos))}</li>
-        </Link>
-      </li>
+      <Card
+        sx={{
+          boxShadow: 5,
+          borderRadius: 1,
+          p: 2
+        }}
+        key={project.teamSeq}
+      >
+        <CardContent>
+          <Stack
+            direction={{ xs: 'column', md: 'row' }}
+            spacing={2}
+            justifyContent="space-between"
+            sx={{ alignItems: 'center' }}
+          >
+            <Stack direction="row" spacing={3} sx={{ alignItems: 'center' }}>
+              <Typography
+                variant="h4"
+                color="primary"
+                noWrap
+                to={`${project.teamSeq}`}
+                component={RouterLink}
+                style={{ textDecoration: 'none' }}
+              >
+                {project.teamName}
+              </Typography>
+              <Typography variant="subtitle1">
+                <Stack direction="row" spacing={2}>
+                  {showTechs}
+                </Stack>
+              </Typography>
+            </Stack>
+            <Stack
+              direction="row"
+              spacing={2}
+              justifyContent="space-between"
+              sx={{ alignItems: 'center' }}
+            >
+              <Typography color="text.secondary" variant="subtitle1">
+                {project.teamManagerName}
+              </Typography>
+              <Typography color="text.secondary">
+                {project.teamMemberCnt}/{project.teamRecruitCnt}
+              </Typography>
+              {showState()}
+            </Stack>
+          </Stack>
+        </CardContent>
+      </Card>
       <br />
     </ul>
   );
